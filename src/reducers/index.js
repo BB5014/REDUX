9f08a22768c05dc4d103d3551c316c1998fafc0b
BB5014 <bblabriz@bbox.fr>
2019-06-04 22:55:39 +0200

    exercice end ok

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,6 @@
 import { combineReducers } from 'redux';
 import areArticlesBeingFetched from './areArticlesBeingFetched';
 import articles from './articles';
-import AppContainer from './App.container';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
-  document.getElementById('root')
-);
 
 export default combineReducers({
   areArticlesBeingFetched,
